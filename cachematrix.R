8717a2e80d781e87c0acae2317f6ebab7a446883
boatkult <adrianzongrone@gmail.com>
2020-09-21 10:56:55 -0400

    Added code to complete each function, and added descriptions for each function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Series of two functions that return the inverse of a matrix,
+## either by calculating with solve() or, if it's already been
+## calculated, by retrieving the cached object.
 
-## Write a short comment describing this function
+## First function takes a matrix ('x') as an argument, and outputs
+## a list of set and get functions to assign and return values
+## for 'x' and its inverse, 'inv'. Set functions assign values for
+## 'x' and 'inv' in the parent environment in order to cache them.
+## If this function is used to set the value of 'x', the value of
+## 'inv' is set to NULL (i.e., the cache is cleared).
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(invmat) inv <<- invmat
+  getinv <- function() inv
+  list(set = set, get = get, setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Second function takes the previous function's output
+## (i.e., a list of get and set functions) as an argument.
+## First uses a get function to retrieve the value for 'inv' from
+## cache and determine whether it's NULL. If it's not NULL, it
+## returns the value for 'inv'. If it is NULL, the inverse matrix
+## is calculated using solve(), and assigned to 'inv'.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
+
+
+
